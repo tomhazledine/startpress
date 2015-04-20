6d6fdac2b5e54d152f89d3eea2d7f1a3e7d87320
Daryll Doyle <daryll@digitalwebmedia.co.uk>
2015-04-20 10:53:28 +0100

    Move scripts to enqueue

--- foot.php
+++ foot.php
@@ -1,9 +1,4 @@
 <div class="footer-meta">
 
-  <div class="scripts">
-    <!-- Javascript -->
-    <script type="text/javascript"  src="<?php echo get_template_directory_uri(); ?>/assets/js/app.js"></script>
-  </div><!-- .scripts -->
-
   <?php wp_footer(); ?>
 </div><!-- .footer-meta -->
--- functions/theme.php
+++ functions/theme.php
@@ -2,31 +2,43 @@
 // Theme-specific functions
 
 // Declare Custom Menus
-  register_nav_menus( array(
-    'header_menu' => 'Header Menu',
-    'footer_menu' => 'Footer Menu'
+register_nav_menus( array(
+  'header_menu' => 'Header Menu',
+  'footer_menu' => 'Footer Menu'
   ) );
 
 // Featured Image Support
-  if ( function_exists( 'add_theme_support' ) ) { 
-    add_theme_support( 'post-thumbnails' );
-    add_image_size( 'thumbnail', 150, 100, true );
-    add_image_size( 'potfolio_s', 238, 119, true );
-    add_image_size( 'mobile_gallery', 1000, 1000, false);
-  }
+if ( function_exists( 'add_theme_support' ) ) { 
+  add_theme_support( 'post-thumbnails' );
+  add_image_size( 'thumbnail', 150, 100, true );
+  add_image_size( 'potfolio_s', 238, 119, true );
+  add_image_size( 'mobile_gallery', 1000, 1000, false);
+}
 
 // Excerpt Length
-  function custom_excerpt_length( $length ) {
-    return 20;
-  }
-  add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+function custom_excerpt_length( $length ) {
+  return 20;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
 // strip width and height from featured images
-  add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 );
-  add_filter( 'image_send_to_editor', 'remove_thumbnail_dimensions', 10 );
-  function remove_thumbnail_dimensions( $html ) {
-      $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
-      return $html;
-  }
+add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 );
+add_filter( 'image_send_to_editor', 'remove_thumbnail_dimensions', 10 );
+function remove_thumbnail_dimensions( $html ) {
+  $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
+  return $html;
+}
 
+// Queue scripts properly
+function my_scripts_method() {
+  if( !is_admin()){
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', ("//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"), false, '1.11.2', true);
+    wp_enqueue_script('jquery');
+
+    wp_register_script('custom-script', get_stylesheet_directory_uri() . '/assets/js/app.js', array( 'jquery' ), false, true);
+    wp_enqueue_script('custom-script');
+  }
+}
+add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
 ?>
