7c2a2e14105bc8cc3a8c25ec9b9994091b6aa508
Tom Hazledine <tom@tomhazledine.com>
2015-05-13 11:33:49 +0100

    Emoji-support removal

--- functions/generic.php
+++ functions/generic.php
@@ -13,6 +13,7 @@
 // 12. Featured Image Caption
 // 13. Allow svg as featured image
 // 14. HTML5 search form
+// 15. Disable the emoji's
 
 // 02. No Self Ping
   function no_self_ping( &$links ) {
@@ -96,4 +97,31 @@
 // 14. HTML5 search form
   add_theme_support( 'html5', array( 'search-form' ) );
 
-?>
+// 15. Disable the emoji's
+function disable_emojis() {
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+}
+add_action( 'init', 'disable_emojis' );
+
+/**
+ * Filter function used to remove the tinymce emoji plugin.
+ *
+ * @param    array  $plugins
+ * @return   array             Difference betwen the two arrays
+ */
+function disable_emojis_tinymce( $plugins ) {
+    if ( is_array( $plugins ) ) {
+        return array_diff( $plugins, array( 'wpemoji' ) );
+    } else {
+        return array();
+    }
+}
